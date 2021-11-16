e45dfe0d831ef00bc06fc86095216d8db6d6a762
GaryTang <gary.tang@citi.com>
2021-11-15 18:34:02 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "time"
+
+    "github.com/go-rod/rod"
+)
+
+func main() {
+    page := rod.New().MustConnect().MustPage("https://www.wikipedia.org/").MustWindowFullscreen()
+
+    page.MustElement("#searchInput").MustInput("earth")
+
+    page.MustWaitLoad().MustScreenshot("a.png")
+    time.Sleep(time.Hour)
+}
